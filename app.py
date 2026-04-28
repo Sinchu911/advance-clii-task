21e4a6b98849071dd2f609329f265a7e736fdc1d
Sinchu <sinchus373@gmail.com>
2026-04-28 23:04:52 +0530

    Update app.run to use environment PORT variable

--- app.py
+++ app.py
@@ -26,5 +26,8 @@ app = create_app()
 with app.app_context():
     db.create_all()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
